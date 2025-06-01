084d91deb1e3921970e06f541a29fbc03f8f4ecf
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-06-01 14:15:42 +0330

    optimizations queries in blog views and add pagination for comments and posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import F
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.utils.text import slugify
 from django.views.generic import ListView, DeleteView, DetailView, CreateView, UpdateView
+from django.core.paginator import Paginator
 
 from .forms import PostCreateForm, CommentCreateForm
 from .models import *
@@ -19,7 +20,7 @@ class PostListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
-        return Post.objects.filter(is_published=True).order_by('-created_at')
+        return Post.objects.filter(is_published=True).select_related('author').order_by('-created_at')
 
 
 class PostDetailView(DetailView):
@@ -29,15 +30,20 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(post=self.object)
+        comments_list = Comment.objects.filter(post=self.object, approved=True).select_related('author')
+        paginator = Paginator(comments_list, 5)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['comments'] = page_obj
         context['comment_form'] = CommentCreateForm()
         return context
 
     def get_object(self):
-        post_pk = self.kwargs.get('post_pk')
-        post_slug = self.kwargs.get('post_slug')
-        post = get_object_or_404(Post, pk=post_pk, slug=post_slug)
-        Post.objects.filter(pk=post.pk).update(views=F('views') + 1)
+        post = Post.objects.prefetch_related('categories', 'tags').get(
+            pk=self.kwargs['post_pk'],
+            slug=self.kwargs['post_slug'],
+        )
+        Post.objects.filter(pk = post.pk).update(views=F('views') + 1)
         post.refresh_from_db()
         return post
 
@@ -68,6 +74,8 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.slug = slugify(form.cleaned_data['title'])
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return self.object.get_absolute_url()
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -83,6 +91,8 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
+    def get_success_url(self):
+        return self.object.get_absolute_url()
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
@@ -92,3 +102,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile')
